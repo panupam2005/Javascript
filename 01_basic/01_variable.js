410cbd3ea315cda10deb8758dd90ee58401e67fc
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-06-26 12:56:13 +0000

    basic

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = "2311601244"
+let accountemail = "ap2005@gmail.com"
+let accountPassword = "12345"
+let accountCity = "Bhopal"
+
+let accountstate
+
+//  console.log(accountCity);
+
+console.table([accountId,accountemail,accountPassword,accountCity,accountstate])
