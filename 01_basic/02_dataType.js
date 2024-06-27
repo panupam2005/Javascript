644e90e16c45bbfabd9df3c984aa91b0b7c19acf
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-06-27 12:14:00 +0000

    datatype

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,26 @@
+let name ="Anupam"
+
+// console.log(name)
+
+// console.log(typeof(name));
+
+istype = Number(name)
+
+// console.log(istype);
+
+value = "33"
+change_value = Number(value)
+
+console.log(change_value);
+
+let str1 = "Anupam"
+let str2 = "Tiwari"
+let str3 = str1 + " " + str2
+
+console.log(str3);
+
+console.log("12" + 12 + 4);
+
+console.log(2 + 4 + "32");
+
+console.log(3 + "6" + 8);
