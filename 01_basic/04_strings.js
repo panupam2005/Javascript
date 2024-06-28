664dc7b8953fa439344c14ecb56b259167cbc6f2
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-06-28 19:36:13 +0000

    string

---
+++ 01_basic/04_strings.js
@@ -0,0 +1,9 @@
+const name = "Anupam"
+const cg = 7.5
+// console.log(name + cg + "in manit");
+
+console.log(`I am ${name} a student of manit bhopal with the cg of ${cg}`);
+
+const url = "https//instoqr20%menu"
+
+console.log(url.replace('20%' , '-'))
