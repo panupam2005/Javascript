c0d2f3abcd97e1c8086624c542442efeda8d5254
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-06-29 10:24:00 +0000

    array1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,23 @@
+// array
+
+// const myarr = [0, 1, 2, 3, 4]
+const myname = ["Anupam", "Tiwari"]
+
+// console.log(myarr[2]);
+
+const myarr = new Array(1, 2, 3, 4, 5)
+console.log(myarr);
+
+// myarr.push(6)
+// myarr.pop(). // last wala remove kar deta hai
+// console.log(myarr);
+
+console.log("A ",myarr);
+
+mys1 = myarr.slice(1,3)
+console.log(mys1);
+console.log("B ",myarr);
+
+mys2 = myarr.splice(1,3)
+console.log(mys2);
+console.log("C ",myarr);
