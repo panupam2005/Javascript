24be2bab5ff6680d28da90141f38fa308d4a450b
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-06-29 09:55:59 +0000

    date

---
+++ 01_basic/05_dates.js
@@ -0,0 +1,13 @@
+let mydate = new Date()
+
+// console.log(mydate.toString());
+console.log(mydate.toDateString());
+
+let mycreateddate = new Date(2024,9,7)
+
+console.log(mycreateddate.toDateString());
+let takedate = new Date()
+
+console.log(takedate.getFullYear());
+console.log(takedate.getDate());
+console.log(takedate.getHours());
