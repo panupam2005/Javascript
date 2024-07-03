4d2d5bc3f3794af66b190d12e37a3bef3335bf2b
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-07-03 19:04:37 +0000

    arrow

---
+++ 03_basic/arrow.js
@@ -0,0 +1,14 @@
+
+const chai = () => {
+    let username = "Anupam"
+    console.log(username);
+}
+
+// chai()
+
+// const adddtwo = (num1,num2) => {
+//     return num1 + num2
+// }
+
+// console.log(adddtwo(5,3))
+
---
+++ 03_basic/scopes.js
@@ -0,0 +1,7 @@
+let a = 30
+if (true) {
+    a = 100
+    // console.log(a);
+}
+
+console.log(a);
