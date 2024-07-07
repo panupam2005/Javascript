1fea495d2bdbf14b417579051b34b81fe06c375e
Anupam Kumar Tiwari <136854013+panupam2005@users.noreply.github.com>
2024-07-07 18:56:29 +0000

    forof

---
+++ iterations/three.js
@@ -0,0 +1,25 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5, 6]
+
+for (const index of arr) {
+    // console.log(index);
+}
+
+const greeting = "Hello World"
+for (const greet of greeting) {
+    // console.log(greet);
+}
+
+// map
+
+const map = new map()
+    map.set('IN', "India")
+    map.set('USA', "United State of America")
+    map.set('FR', "France")
+    map.set('IN', "India")
+
+
+for (const key of map) {
+    console.log(key, ':-', value);
+}
